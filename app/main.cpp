eb1f2f65da14de36b4b8b44f5f15d0b011bbc2ab
Ian Kim <ian@ianmkim.com>
2022-12-24 14:05:46 -0500

    poc loading elf files

--- app/main.cpp
+++ app/main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 
-int main(){
+#include <elfio/elfio.hpp>
+
+int main(int argc, char** argv){
+    ELFIO::elfio reader;
+    if(!reader.load("../rv32ui-p-add")){
+        std::cout << "Cannot find or process ELF file" << std::endl;
+        return 2;
+    }
+
+    ELFIO::Elf_Half sec_num = reader.sections.size();
+    std::cout << "Number of sections: " << sec_num << std::endl;
+    for(int i = 0; i < sec_num; i++){
+        const ELFIO::section* psec = reader.sections[i];
+        std::cout << "  [" << i << "] "
+              << psec->get_name()
+              << "\t"
+              << psec->get_size()
+              << std::endl;
+
+        const char* p = reader.sections[i]->get_data();
+    }
+    
     return 0;
 }
